fd43f5fc220f6640499a5a4a399970bf51ce9283
Arthitphk <128705950+Arthitphk@users.noreply.github.com>
2026-01-22 18:00:32 +0700

    add more toggle

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,17 @@
+// Hamberger 
+
 const hamburger  = document.querySelector('.hamburger');
 const navbar = document.querySelector('.nav-menu');
 
 
 hamburger.addEventListener('click', ()=> {
-    hamburger.classList.add('active');
-    navbar.classList.add('active');
+    hamburger.classList.toggle('active');
+    navbar.classList.toggle('active');
 })
+
+document.querySelectorAll('.menu-item')
+.forEach(item => item.addEventListener('click', ()=> {
+    hamburger.classList.remove('active');
+    navbar.classList.remove('active');
+}));
+    
